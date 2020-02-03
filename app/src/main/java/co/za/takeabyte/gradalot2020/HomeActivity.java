be9abd3ace20657428e12e998eb0dfdc814addb5
jasonbiel <jason.biel@takealot.com>
2020-02-03 08:24:38 +0200

    Gradalot2020 - java project setup files.

---
+++ app/src/main/java/co/za/takeabyte/gradalot2020/HomeActivity.java
@@ -0,0 +1,6 @@
+package co.za.takeabyte.gradalot2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+}
--- app/src/main/java/co/za/takeabyte/gradalot2020/HomeActivity.kt
+++
@@ -1,5 +0,0 @@
-package co.za.takeabyte.gradalot2020
-
-import androidx.appcompat.app.AppCompatActivity
-
-class HomeActivity : AppCompatActivity()
